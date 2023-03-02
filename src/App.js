25db07ed8768b488baef79347b67baac80fa3ca5
ElbaKokaj <elbaaak@gmail.com>
2023-03-02 15:09:44 +0100

    fillimi i student page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
-
+import Student from './components/Student';
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-        {/* Add other routes for other pages here */}
+        {/* <Route exact path="/" element={<Login />} /> */}
+        {/* <Route path="/dashboard/student" component={Student} /> qeshtu duhet mu bo mbasi e lidh log in me student po nihere po e ndreqim faqen e student */}
+        <Route exact path="/" element={<Student />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/Student.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import '../css/Student.css';
+
+const Student = ({ name, picture }) => {
+    return (
+        <div className='student-page'>
+            <div className='header'>
+                <img className='profile-pic' src={picture} alt='Profile' />
+                <h1>Hello {name}!</h1>
+            </div>
+            <nav className='nav-bar'>
+                <ul>
+                    <li>
+                        <NavLink exact to='/profile' activeClassName='active-link'>
+                            Profile
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink exact to='/courses' activeClassName='active-link'>
+                            Courses
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink exact to='/complain' activeClassName='active-link'>
+                            Complain
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink exact to='/notice' activeClassName='active-link'>
+                            Notice
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink exact to='/calendar' activeClassName='active-link'>
+                            Calendar
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink exact to='/logout' activeClassName='active-link'>
+                            Logout
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+            <div className='content'>
+                <p>Welcome to your student dashboard!</p>
+                <p>Here you can access all of your important information and resources.</p>
+            </div>
+        </div>
+    );
+};
+
+export default Student;
