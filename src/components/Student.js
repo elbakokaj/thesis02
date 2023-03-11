0f77fcead331625c98c73f917ad14dabeccd4131
ElbaKokaj <elbaaak@gmail.com>
2023-03-11 15:00:47 +0100

    faza e pare e student done

--- src/components/Student.js
+++ src/components/Student.js
@@ -4,48 +4,53 @@ import '../css/Student.css';
 
 const Student = ({ name, picture }) => {
     return (
-        <div className='student-page'>
-            <div className='header'>
-                <img className='profile-pic' src={picture} alt='Profile' />
-                <h1>Hello {name}!</h1>
+        <div className='studentPage'>
+
+            <div className='dashboard'>
+                <header>
+                    <img className='profile-pic' src={picture} alt='Profile' />
+                    <h1>Hello {name}!</h1>
+                </header>
+                <nav>
+                    <ul>
+                        <li>
+                            <NavLink exact to='/profile' activeClassName='active-link'>
+                                Profile
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink exact to='/courses' activeClassName='active-link'>
+                                Courses
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink exact to='/complain' activeClassName='active-link'>
+                                Complain
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink exact to='/notice' activeClassName='active-link'>
+                                Notice
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink exact to='/calendar' activeClassName='active-link'>
+                                Calendar
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink exact to='/logout' activeClassName='active-link'>
+                                Logout
+                            </NavLink>
+                        </li>
+                    </ul>
+                </nav>
             </div>
-            <nav className='nav-bar'>
-                <ul>
-                    <li>
-                        <NavLink exact to='/profile' activeClassName='active-link'>
-                            Profile
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink exact to='/courses' activeClassName='active-link'>
-                            Courses
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink exact to='/complain' activeClassName='active-link'>
-                            Complain
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink exact to='/notice' activeClassName='active-link'>
-                            Notice
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink exact to='/calendar' activeClassName='active-link'>
-                            Calendar
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink exact to='/logout' activeClassName='active-link'>
-                            Logout
-                        </NavLink>
-                    </li>
-                </ul>
-            </nav>
             <div className='content'>
-                <p>Welcome to your student dashboard!</p>
-                <p>Here you can access all of your important information and resources.</p>
+                <div className='content-text'>
+                    <p>Welcome to your student dashboard!</p>
+                    <p>Here you can access all of your important information and resources.</p>
+                </div>
             </div>
         </div>
     );
